8898fffe32fd851a8b99cc318b345380dc5285f2
Pepe Cano <825430+ppcano@users.noreply.github.com>
2024-04-02 16:14:01 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+const failRate = new Rate('failed_requests');
+
+export const options = {
+  thresholds: {
+    failed_requests: ['rate<=0'],
+    http_req_duration: ['p(95)<500'],
+  }
+};
+
+export default function () {
+  const result = http.get('https://test-api.k6.io');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
+  failRate.add(result.status !== 200);
+}
